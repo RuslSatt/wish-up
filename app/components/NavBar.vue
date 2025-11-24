237237d515a64a4cbce28ea344fbb409aff77d09
RuslSatt <r8sattarov@mail.ru>
2025-11-25 01:07:48 +0500

    feat(NavBar): add actions for wishlist and gift with drawer functionality

--- app/components/NavBar.vue
+++ app/components/NavBar.vue
@@ -10,6 +10,17 @@ const tools = ref([
   { route: 'profile', icon: 'i-lucide-user' },
 ])
 
+const actions = ref([
+  {
+    name: 'Список желаний',
+    icon: 'i-lucide-layout-template',
+  },
+  {
+    name: 'Подарок',
+    icon: 'i-lucide-gift',
+  },
+])
+
 const isActive = (toolRoute: string) => {
   if (toolRoute === 'dashboard') {
     return route.name === 'dashboard'
@@ -18,9 +29,6 @@ const isActive = (toolRoute: string) => {
 }
 
 const navigateTo = (toolRoute: string) => {
-  if (toolRoute === 'add') {
-    return
-  }
   router.push(`/${toolRoute}`)
 }
 </script>
@@ -30,14 +38,41 @@ const navigateTo = (toolRoute: string) => {
     <ul class="flex items-center justify-around">
       <li v-for="tool in tools" :key="tool.icon">
         <UButton
+          v-if="tool.route !== 'add'"
           :class="{
             'text-black dark:text-white': isActive(tool.route),
             'text-gray-400 dark:text-gray-600': !isActive(tool.route),
           }"
-          :variant="tool.route === 'add' ? 'outline' : 'link'"
+          variant="link"
           :icon="tool.icon"
           @click="navigateTo(tool.route)"
         />
+
+        <UDrawer v-if="tool.route === 'add'">
+          <UButton variant="outline" :icon="tool.icon" />
+
+          <template #content>
+            <div class="flex flex-col items-center justify-center h-full w-full p-2 pb-5 gap-5">
+              <h3 class="mt-auto text-xs">Создайте что-нибудь</h3>
+              <ul class="flex gap-10">
+                <li
+                  v-for="action in actions"
+                  :key="action.name"
+                  class="flex flex-col items-center gap-1"
+                >
+                  <UButton
+                    color="neutral"
+                    variant="outline"
+                    :icon="action.icon"
+                    size="xl"
+                    :title="action.name"
+                  />
+                  <span class="text-[0.5rem]">{{ action.name }}</span>
+                </li>
+              </ul>
+            </div>
+          </template>
+        </UDrawer>
       </li>
     </ul>
   </nav>
