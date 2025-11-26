47408a89f5d48356bdb3b7dbed20db33cb98c1ea
RuslSatt <r8sattarov@mail.ru>
2025-11-27 00:38:48 +0500

    refactor(profile): replace user profile layout with reusable components for header and user info

---
+++ app/components/ProfileHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="flex items-center w-full">
+    <UButton
+      class="justify-items-start"
+      icon="i-lucide-arrow-left"
+      variant="link"
+      color="neutral"
+    />
+    <h1 class="flex-1 text-center">Профиль</h1>
+    <UButton class="justify-items-start" icon="i-lucide-settings" variant="link" color="neutral" />
+  </header>
+</template>
---
+++ app/components/ProfileUserInfo.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+const user = useSupabaseUser()
+</script>
+
+<template>
+  <div class="flex flex-col items-center gap-3 justify-center pt-5">
+    <UAvatar src="/images/ava.jpg" size="3xl" />
+    <p>{{ user?.email }}</p>
+    <div class="flex gap-2 w-full justify-center items-center">
+      <UButton variant="subtle" color="neutral" class="flex-1 flex justify-center" size="sm"
+        >Редактировать</UButton
+      >
+      <UButton variant="subtle" color="neutral" class="flex-1 flex justify-center" size="sm"
+        >Поделиться</UButton
+      >
+    </div>
+  </div>
+</template>
